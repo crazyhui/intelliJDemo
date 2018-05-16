9f3063df9a6715e2dd42abb71bfc57e4842eb7de
chenhui <2475834034@qq.com>
2018-05-16 14:26:10 +0800

    Initial commit

---
+++ src/com/imooc/test/MyTest.java
@@ -0,0 +1,18 @@
+package com.imooc.test;
+
+public class MyTest {
+    //psvm  main()
+    //打印输出 sout
+    public static void main(String[] args) {
+        String a = "hao";
+        System.out.println("开始进行调试");
+        for (int i = 0; i < 4; i++) {
+            print();
+            a = a + " "+i;
+        }
+    }
+
+    private static void print() {
+        System.out.println("请叫我大师兄");
+    }
+}
